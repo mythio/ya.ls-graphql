0eedd7bedd3c10ae4ec3398586bc56df9438cb21
mythio <mythio.2909@gmail.com>
2020-02-01 03:10:54 +0530

    [chore]: Add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    req.isAuth = false;
+    return next;
+  }
+  const token = authHeader;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, "secret");
+  } catch (err) {
+    req.isAuth = false;
+    console.log(err);
+    return next();
+  }
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
+};
