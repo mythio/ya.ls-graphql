4c54193277469002c2902693ea4cb1556c968efa
mythio <mythio.2909@gmail.com>
2020-02-20 06:46:01 +0530

    [test]: Test for query.me

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -25,7 +25,7 @@ describe("Resolver", () => {
 
       await user.save();
 
-      const q = gql`
+      const QUERY_ME = gql`
         query {
           me {
             userId
@@ -44,9 +44,95 @@ describe("Resolver", () => {
       const server = serverInit({ authorization: token });
 
       const { query } = createTestClient(server);
-      const res = await query({ query: q });
+      const res = await query({ query: QUERY_ME });
       expect(res.errors).toBeUndefined();
       expect(res.data).toMatchSnapshot();
+    });
+
+    test("should return `not authorized` for invalid userId", async () => {
+      const user = new User({
+        _id: "5e4dcdfcc76d441afd3d29d9",
+        name: "user_name",
+        emailAddress: "email@address.com",
+        password: "123123123",
+        shortIds: []
+      });
+
+      await user.save();
+
+      const QUERY_ME = gql`
+        query {
+          me {
+            userId
+            name
+            emailAddress
+            shortIds {
+              shortId
+              originalUrl
+            }
+          }
+        }
+      `;
+
+      const token = jwt.sign(
+        { userId: "5e4dcdfcc76d441afd3d29d6" },
+        process.env.USER_SECRET
+      );
+
+      const server = serverInit({ authorization: token });
+
+      const { query } = createTestClient(server);
+      const res = await query({ query: QUERY_ME });
+
+      expect(res.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ message: "not authorized" })
+        ])
+      );
+      expect(res.data).toBeNull();
+    });
+
+    test("should return `not authorized` for invalid token", async () => {
+      const user = new User({
+        _id: "5e4dcdfcc76d441afd3d29d9",
+        name: "user_name",
+        emailAddress: "email@address.com",
+        password: "123123123",
+        shortIds: []
+      });
+
+      await user.save();
+
+      const QUERY_ME = gql`
+        query {
+          me {
+            userId
+            name
+            emailAddress
+            shortIds {
+              shortId
+              originalUrl
+            }
+          }
+        }
+      `;
+
+      const token = jwt.sign(
+        { userId: "5e4dcdfcc76d441afd3d29d9" },
+        "wrongsecret"
+      );
+
+      const server = serverInit({ authorization: token });
+
+      const { query } = createTestClient(server);
+      const res = await query({ query: QUERY_ME });
+
+      expect(res.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ message: "not authorized" })
+        ])
+      );
+      expect(res.data).toBeNull();
     });
   });
 });
