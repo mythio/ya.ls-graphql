2a20efedd3daed217bff953e7c66a91ea8108219
mythio <mythio.2909@gmail.com>
2020-04-16 16:52:46 +0530

    chore: Init and add dependencies

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:13
-USER node
-RUN mkdir -p /home/node/app && chown -R node:node /home/node/app
-WORKDIR /home/node/app
-COPY --chown=node:node . .
-RUN npm install
-EXPOSE 3000
-CMD [ "npm", "start" ]
---
+++ webpack.production.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const nodeExternals = require("webpack-node-externals");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  devtool: "source-map",
+  entry: [path.join(__dirname, "src/main.ts")],
+  externals: [nodeExternals({})],
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+});
