fb1b99edfac641c2a66d38e05b24ca600a2847b0
mythio <mythio.2909@gmail.com>
2020-04-19 12:45:58 +0530

    chore: Add tests

---
+++ tests/core/JWT/unit.test.ts
@@ -0,0 +1,32 @@
+import { readFileSpy } from './mock';
+import JWT, { JwtPayload } from '../../../src/core/JWT';
+
+describe(`JWT class tests`, () => {
+
+  beforeEach(() => {
+    readFileSpy.mockClear();
+  });
+
+  const issuer = 'issuer';
+  const audience = 'audience';
+  const subject = 'subject';
+  const param = 'param';
+  const validity = 1;
+
+  it(`should throw error for invalid token in JWT.decode`, async () => {
+    try {
+      await JWT.decode('abc');
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+    expect(readFileSpy).toBeCalledTimes(1);
+  });
+
+  it('Should generate a token for JWT.encode', async () => {
+    const payload = new JwtPayload(issuer, audience, subject, param, validity);
+    const token = await JWT.encode(payload);
+
+    expect(typeof token).toBe('string');
+    expect(readFileSpy).toBeCalledTimes(1);
+  });
+})
